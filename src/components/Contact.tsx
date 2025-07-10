c902f58d738956ab27a25382329abcad8ee7a8d8
Michal Gawik <gawlik.zator@o2.pl>
2025-07-10 14:22:36 +0200

    restyle

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,30 +1,32 @@
 import { useTranslation } from 'react-i18next'
 import { motion } from 'framer-motion'
+import Footer from './Footer'
 const Contact = () => {
 	const { t } = useTranslation()
 	return (
-		<motion.section
-			initial={{ x: '150%' }}
-			animate={{ x: 0 }}
-			exit={{ x: '150%' }}
-			transition={{ duration: 0.3, delay: 0.2 }}
-			style={{
-				background: `
-  radial-gradient(circle at 90% -40%, #ee79de 0%, transparent 48%),
-  radial-gradient(circle at 30% 150%, #ee79de 0%, #000 48%)
+		<>
+			<motion.section
+				style={{
+					background: `
+radial-gradient(circle at 30% -30%, #131213 0%, transparent 48%),
+  radial-gradient(circle at 100% 110%, #4b1544 0%, #000 28%)
 `,
-			}}
-			className='h-screen mt-20 p-4 md:p-10 rounded-sm flex flex-col justify-center max-w-screen shadow-sm mb-10'>
-			<h2 className='text-white text-4xl sm:text-5xl lg:text-6xl 2xl:text-7xl 3xl:text-8xl font-bold tracking-wide capitalize mb-10'>
-				{t('contactTitle')}
-			</h2>
-			<h3 className=' text-white text-3xl sm:text-5xl lg:text-6xl 2xl:text-7xl 3xl:text-8xl font-bold tracking-wide mb-6'>
-				gawlik.zator@o2.pl
-			</h3>
-			<h3 className=' text-white text-3xl sm:text-5xl lg:text-6xl 2xl:text-7xl 3xl:text-8xl font-bold tracking-wide '>
-				+48 512-374-866
-			</h3>
-		</motion.section>
+				}}
+				className='max-w-screen min-h-screen py-20'>
+				{/* <h2 className='text-white text-4xl sm:text-5xl lg:text-6xl 2xl:text-7xl 3xl:text-8xl font-bold tracking-wide capitalize mb-10'>
+					{t('contactTitle')}
+				</h2> */}
+				<div className='w-[80vw] lg:w-[60vw] h-[80vh] mx-auto flex flex-col justify-center'>
+					<h3 className=' text-white text-2xl sm:text-4xl lg:text-5xl 2xl:text-6xl 3xl:text-7xl font-bold tracking-wide mb-6'>
+						gawlik.zator@o2.pl
+					</h3>
+					<h3 className=' text-white text-2xl sm:text-4xl lg:text-5xl 2xl:text-6xl 3xl:text-7xl font-bold tracking-wide '>
+						+48 512-374-866
+					</h3>
+				</div>
+			</motion.section>
+			<Footer />
+		</>
 	)
 }
 
