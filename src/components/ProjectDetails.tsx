88ef0ed63dcc8a49cc9c457b22bbc3fa181f844a
Michal Gawik <gawlik.zator@o2.pl>
2025-07-10 14:08:11 +0200

    add footer

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -12,6 +12,7 @@ import {
 	TooltipProvider,
 	TooltipTrigger,
 } from '@/components/ui/tooltip'
+import Footer from './Footer'
 
 const ProjectDetails = () => {
 	const { t, i18n } = useTranslation()
@@ -36,106 +37,109 @@ const ProjectDetails = () => {
 	}
 
 	return (
-		<motion.div
-			initial={{ x: '-200%' }}
-			animate={{ x: 0 }}
-			exit={{ x: '-200%' }}
-			transition={{ duration: 0.3 }}
-			style={{
-				background: `
+		<>
+			<motion.div
+				initial={{ x: '-200%' }}
+				animate={{ x: 0 }}
+				exit={{ x: '-200%' }}
+				transition={{ duration: 0.3 }}
+				style={{
+					background: `
   radial-gradient(circle at 30% -30%, #131213 0%, transparent 48%),
   radial-gradient(circle at 100% 110%, #4b1544 0%, #000 28%)
 `,
-			}}
-			className='flex flex-col max-w-screen min-h-screen py-20'>
-			<Link
-				to='/projects'
-				className='absolute top-4 left-8 text-gray-500 hover:text-gray-800 duration-300 flex items-center group 3xl:text-2xl'>
-				<FaArrowLeft className='mr-2 transition-transform duration-300 group-hover:scale-125 group-hover:-translate-x-2' />
-				{t('goBackBtn')}
-			</Link>
-			<div className='mb-8 w-[80vw] lg:w-[60vw] mx-auto text-slate-100 '>
-				<h2 className='text-3xl md:text-4xl font-bold tracking-wide capitalize 3xl:text-5xl'>
-					{project.title}
-				</h2>
+				}}
+				className='flex flex-col max-w-screen min-h-screen py-20'>
+				<Link
+					to='/projects'
+					className='absolute top-4 left-8 text-gray-500 hover:text-gray-800 duration-300 flex items-center group 3xl:text-2xl'>
+					<FaArrowLeft className='mr-2 transition-transform duration-300 group-hover:scale-125 group-hover:-translate-x-2' />
+					{t('goBackBtn')}
+				</Link>
+				<div className='mb-8 w-[80vw] lg:w-[60vw] mx-auto text-slate-100 '>
+					<h2 className='text-3xl md:text-4xl font-bold tracking-wide capitalize 3xl:text-5xl'>
+						{project.title}
+					</h2>
 
-				<div className='flex flex-wrap mx-auto justify-center gap-x-8  mt-14'>
-					<p className='text-4xl sm:text-6xl text-slate-100 '></p>
-					{project.tech.map((IconComponent, index) => (
-						<div key={index} className='text-4xl sm:text-6xl text-slate-100 '>
-							<IconComponent />
-						</div>
-					))}
-				</div>
-				<div className='bg-slate-100 rounded-sm shadow-sm p-5 sm:p-10 mt-10 max-w-full'>
-					<p className='text-xl 3xl:text-2xl  3xl:leading-10 leading-loose text-justify text-gray-900'>
-						{project.text[i18n.language as keyof typeof project.text]}
-					</p>
-					<div className='flex gap-x-12 justify-center my-8'>
-						{project.url ? (
-							<TooltipProvider>
-								<Tooltip>
-									<TooltipTrigger>
-										<a target='_blank' href={project.url} className=''>
-											<TbWorldWww className='h-14 w-14 3xl:w-20 3xl:h-20 text-sky-950 hover:text-purple-950 duration-300 hover:scale-110' />
-										</a>
-									</TooltipTrigger>
-									<TooltipContent>
-										<p className='text-lg md:text-xl'>Live Version</p>
-									</TooltipContent>
-								</Tooltip>
-							</TooltipProvider>
-						) : (
-							''
-						)}
-
-						{project.adminUrl ? (
-							<TooltipProvider>
-								<Tooltip>
-									<TooltipTrigger>
-										<a target='_blank' href={project.adminUrl} className=''>
-											<RiAdminFill className='h-14 w-14 3xl:w-20 3xl:h-20 text-sky-950 hover:text-purple-950 duration-300 hover:scale-110' />
-										</a>
-									</TooltipTrigger>
-									<TooltipContent>
-										<p className='text-lg md:text-xl'>Admin Panel</p>
-									</TooltipContent>
-								</Tooltip>
-							</TooltipProvider>
-						) : (
-							''
-						)}
-
-						<TooltipProvider>
-							<Tooltip>
-								<TooltipTrigger>
-									<a target='_blank' href={project.github} className=''>
-										<FaGithub className='h-14 w-14 3xl:w-20 3xl:h-20 text-sky-950 hover:text-purple-950 duration-300 hover:scale-110' />
-									</a>
-								</TooltipTrigger>
-								<TooltipContent>
-									<p className='text-lg md:text-xl'>Github</p>
-								</TooltipContent>
-							</Tooltip>
-						</TooltipProvider>
-					</div>
-					<p className='text-xl 3xl:text-2xl leading-loose mt-2 text-gray-900'>
-						{t('projectTech')}:{' '}
-						{project?.techText?.map((tech, index) => (
-							<span
-								className={`font-bold tracking-wide max-w-full inline-block  ${
-									index < project.techText.length - 1
-										? "after:content-[','] after:mr-1"
-										: ''
-								}`}
-								key={index}>
-								{tech}
-							</span>
+					<div className='flex flex-wrap mx-auto justify-center gap-x-8  mt-14'>
+						<p className='text-4xl sm:text-6xl text-slate-100 '></p>
+						{project.tech.map((IconComponent, index) => (
+							<div key={index} className='text-4xl sm:text-6xl text-slate-100 '>
+								<IconComponent />
+							</div>
 						))}
-					</p>
+					</div>
+					<div className='bg-slate-100 rounded-sm shadow-sm p-5 sm:p-10 mt-10 max-w-full'>
+						<p className='text-xl 3xl:text-2xl  3xl:leading-10 leading-loose text-justify text-gray-900'>
+							{project.text[i18n.language as keyof typeof project.text]}
+						</p>
+						<div className='flex gap-x-12 justify-center my-8'>
+							{project.url ? (
+								<TooltipProvider>
+									<Tooltip>
+										<TooltipTrigger>
+											<a target='_blank' href={project.url} className=''>
+												<TbWorldWww className='h-14 w-14 3xl:w-20 3xl:h-20 text-sky-950 hover:text-purple-950 duration-300 hover:scale-110' />
+											</a>
+										</TooltipTrigger>
+										<TooltipContent>
+											<p className='text-lg md:text-xl'>Live Version</p>
+										</TooltipContent>
+									</Tooltip>
+								</TooltipProvider>
+							) : (
+								''
+							)}
+
+							{project.adminUrl ? (
+								<TooltipProvider>
+									<Tooltip>
+										<TooltipTrigger>
+											<a target='_blank' href={project.adminUrl} className=''>
+												<RiAdminFill className='h-14 w-14 3xl:w-20 3xl:h-20 text-sky-950 hover:text-purple-950 duration-300 hover:scale-110' />
+											</a>
+										</TooltipTrigger>
+										<TooltipContent>
+											<p className='text-lg md:text-xl'>Admin Panel</p>
+										</TooltipContent>
+									</Tooltip>
+								</TooltipProvider>
+							) : (
+								''
+							)}
+
+							<TooltipProvider>
+								<Tooltip>
+									<TooltipTrigger>
+										<a target='_blank' href={project.github} className=''>
+											<FaGithub className='h-14 w-14 3xl:w-20 3xl:h-20 text-sky-950 hover:text-purple-950 duration-300 hover:scale-110' />
+										</a>
+									</TooltipTrigger>
+									<TooltipContent>
+										<p className='text-lg md:text-xl'>Github</p>
+									</TooltipContent>
+								</Tooltip>
+							</TooltipProvider>
+						</div>
+						<p className='text-xl 3xl:text-2xl leading-loose mt-2 text-gray-900'>
+							{t('projectTech')}:{' '}
+							{project?.techText?.map((tech, index) => (
+								<span
+									className={`font-bold tracking-wide max-w-full inline-block  ${
+										index < project.techText.length - 1
+											? "after:content-[','] after:mr-1"
+											: ''
+									}`}
+									key={index}>
+									{tech}
+								</span>
+							))}
+						</p>
+					</div>
 				</div>
-			</div>
-		</motion.div>
+			</motion.div>
+			<Footer />
+		</>
 	)
 }
 
