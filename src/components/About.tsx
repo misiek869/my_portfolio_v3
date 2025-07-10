94b3d9b115070a34cd80df242701465fa954c867
Michal Gawik <gawlik.zator@o2.pl>
2025-07-10 08:57:08 +0200

    restyle

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next'
 import { motion } from 'framer-motion'
 import Skills from './Skills'
 import Navigation from './Navigation'
+import Footer from './Footer'
 
 const About = () => {
 	const { t } = useTranslation()
@@ -14,7 +15,7 @@ const About = () => {
   radial-gradient(circle at 100% 110%, #4b1544 0%, #000 28%)
 `,
 			}}
-			className='max-w-screen min-h-screen'>
+			className='max-w-screen min-h-screen py-20'>
 			<motion.div
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
@@ -26,7 +27,7 @@ const About = () => {
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
 				transition={{ delay: 0.1, duration: 0.7 }}
-				className='mb-8 pt-20 w-[80vw] lg:w-[60vw] mx-auto text-slate-100 '>
+				className='mb-8  w-[80vw] lg:w-[60vw] mx-auto text-slate-100 '>
 				<h2 className=' text-3xl md:text-4xl font-bold tracking-wide capitalize 3xl:text-5xl'>
 					{t('aboutTitle')}
 				</h2>
@@ -53,6 +54,7 @@ const About = () => {
 			</motion.div>
 
 			<Skills />
+			<Footer />
 		</section>
 	)
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -35,28 +35,22 @@ const skills: Skill[] = [
 const Skills = () => {
 	const { t } = useTranslation()
 	return (
-		<div>
-			<h2 className='bg-gradient-to-r from-gray-950  to-purple-950 inline-block text-transparent bg-clip-text text-2xl md:text-3xl font-bold tracking-wide capitalize 3xl:text-4xl mb-12'>
+		<div className='w-[80vw] lg:w-[60vw] mx-auto mt-20'>
+			<h2 className='text-slate-100 text-3xl md:text-4xl font-bold tracking-wide capitalize 3xl:text-5xl mb-12'>
 				{t('skillsTitle')}
 			</h2>
-			<div className='flex flex-wrap gap-6  justify-center mx-auto'>
+			<div className='grid grid-cols-2 md:grid-cols-4 gap-12 items-center place-items-center w-[80%] lg:w-[60%] mx-auto'>
 				{skills.map((item, index) => {
 					return (
 						<TooltipProvider key={index}>
 							<Tooltip>
-								<TooltipTrigger className='w-16 h-16 rounded-full flex items-center justify-center bg-slate-50 group'>
-									<div className='text-3xl group-hover:text-gray-700 transition-all duration-300'>
+								<TooltipTrigger className='w-16 h-16 md:w-24 md:h-24 rounded-full flex items-center justify-center bg-slate-100 group'>
+									<div className='text-3xl md:text-5xl group-hover:text-gray-700 transition-all duration-300'>
 										{item.icon}
 									</div>
 								</TooltipTrigger>
-								<TooltipContent
-									style={{
-										background: `
-  radial-gradient(circle at 30% -30%, #252225 0%, transparent 48%),
-  radial-gradient(circle at 90% 150%, #aa559f 0%, #000 55%)
-`,
-									}}>
-									<p className='text-lg '>{item.name}</p>
+								<TooltipContent className='bg-slate-100 '>
+									<p className='text-lg text-gray-800'>{item.name}</p>
 								</TooltipContent>
 							</Tooltip>
 						</TooltipProvider>
