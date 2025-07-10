910a5c32102d3d372e61caea8f082f376247ad8f
Michal Gawik <gawlik.zator@o2.pl>
2025-07-10 12:58:05 +0200

    create

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
 import { useTranslation } from 'react-i18next'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+import certificateImg from '../assets/images/devmentor-certificate.jpg'
 
 const Education = () => {
 	const { t } = useTranslation()
@@ -8,23 +10,30 @@ const Education = () => {
 			<h2 className='text-slate-100 text-3xl md:text-4xl font-bold tracking-wide capitalize 3xl:text-5xl mb-12'>
 				{t('educationTitle')}
 			</h2>
-			<div className='text-slate-100 border p-6 rounded-sm '>
-				<div className=''>
-					<h2 className='text-2xl md:text-3xl font-semibold mb-4 tracking-wider'>
+			<div className='text-slate-100 border p-6 rounded-sm gap-6 flex flex-col 2xl:flex-row'>
+				<div className='w-full 2xl:w-[80%]'>
+					<h2 className='text-xl md:text-2xl 2xl:text-3xl font-semibold mb-4 tracking-wider'>
 						Mentoring frontend - JavaScript, React
 					</h2>
 					<div className='flex gap-10 mb-3'>
-						<p className='font-bold text-xl md:text-2xl tracking-wider'>
+						<p className='font-bold text-lg md:text-xl 2xl:text-2xl tracking-wider'>
 							Start: <span className='font-normal'>09.2023</span>{' '}
 						</p>
-						<p className='font-bold text-xl md:text-2xl tracking-wider'>
+						<p className='font-bold text-lg md:text-xl 2xl:text-2xl tracking-wider'>
 							Koniec: <span className='font-normal'>03.2024</span>
 						</p>
 					</div>
-					<p className='tracking-wider'>{t('mentoringDescription')}</p>
+					<h3 className='mb-3 font-bold text-lg md:text-xl 2xl:text-2xl tracking-wider'>
+						Organizator: <span className='font-normal'>DevMentor.pl</span>
+					</h3>
+					<p className='tracking-wider text-justify text-md md:text-lg lg:text-xl leading-loose'>
+						{t('mentoringDescription')}
+					</p>
 				</div>
-				<div className=''>
-					<img src='' alt='' />
+				<div className='mx-auto my-auto'>
+					<Zoom>
+						<img src={certificateImg} alt='' className='h-[400px]' />
+					</Zoom>
 				</div>
 			</div>
 		</div>
