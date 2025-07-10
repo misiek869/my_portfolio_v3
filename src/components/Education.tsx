96c6f5012a71e0ddf28760df403a4f786b854453
Michal Gawik <gawlik.zator@o2.pl>
2025-07-10 11:14:28 +0200

    create component

---
+++ src/components/Education.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+const Education = () => {
+	const { t } = useTranslation()
+	return (
+		<div className='w-[80vw] lg:w-[60vw] mx-auto mt-20 pb-20'>
+			<h2 className='text-slate-100 text-3xl md:text-4xl font-bold tracking-wide capitalize 3xl:text-5xl mb-12'>
+				{t('skillsTitle')}
+			</h2>
+		</div>
+	)
+}
+
+export default Education
