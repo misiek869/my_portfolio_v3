abdb4c0ef6bf9c1eb3d6a67fda385c66e0db1f75
Michal Gawik <gawlik.zator@o2.pl>
2025-07-11 06:02:50 +0200

    add photo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,6 +4,7 @@ import Skills from './Skills'
 import Navigation from './Navigation'
 import Footer from './Footer'
 import Education from './Education'
+import myPhoto from '../assets/images/michal.jpg'
 
 const About = () => {
 	const { t } = useTranslation()
@@ -35,14 +36,35 @@ const About = () => {
 					<h2 className=' text-3xl md:text-4xl font-bold tracking-wide capitalize 3xl:text-5xl mb-10'>
 						{t('aboutTitle')}
 					</h2>
-					<div className=''>
+					<div className='relative'>
+						<div className='flex flex-wrap'>
+							<img
+								src={myPhoto}
+								alt='Opis zdjęcia'
+								className='w-full sm:w-1/3 pr-4 pb-4 float-right'
+							/>
+							<div className='flex-1 min-w-[60%]'>
+								<p className={paragraphStyles}>{t('about_01')}</p>
+								<p className={paragraphStyles}>{t('about_02')}</p>
+							</div>
+						</div>
+
+						{/* Pełna szerokość od tego momentu */}
+						<p className={paragraphStyles}>{t('about_03')}</p>
+						<p className={paragraphStyles}>{t('about_04')}</p>
+						<p className={paragraphStyles}>{t('about_05')}</p>
+						<p className={paragraphStyles}>{t('about_06')}</p>
+						{/* ... */}
+					</div>
+
+					{/* <div className=''>
 						<p className={paragraphStyles}>{t('about_01')}</p>
 						<p className={paragraphStyles}>{t('about_02')}</p>
 						<p className={paragraphStyles}>{t('about_03')}</p>
 						<p className={paragraphStyles}>{t('about_04')}</p>
 						<p className={paragraphStyles}>{t('about_05')}</p>
 						<p className={paragraphStyles}>{t('about_06')}</p>
-					</div>
+					</div> */}
 				</motion.div>
 
 				<Skills />
