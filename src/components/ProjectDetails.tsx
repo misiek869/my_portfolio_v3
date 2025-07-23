79b3fa2c94a9f020c8d5b4be3fc241f15d9ac3f7
Michal Gawik <gawlik.zator@o2.pl>
2025-07-23 06:11:57 +0200

    change padding

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -76,7 +76,7 @@ const ProjectDetails = () => {
   radial-gradient(circle at 100% 110%, #2c2c2c 5%, #494949 40%)
 `,
 						}}
-						className=' rounded-xl shadow-sm p-5 sm:p-10 mt-10 max-w-full'>
+						className=' rounded-xl shadow-sm p-10 md:p-16 mt-10 max-w-full'>
 						<p className='text-xl 3xl:text-2xl  3xl:leading-10 leading-loose text-justify text-slate-50'>
 							{project.text[i18n.language as keyof typeof project.text]}
 						</p>
