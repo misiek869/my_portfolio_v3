31f8c804999aa800ba806b9eb8cbc8e6f37621e6
Michal Gawik <gawlik.zator@o2.pl>
2025-07-23 06:17:35 +0200

    add social icons

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@
 import { motion } from 'framer-motion'
 import Footer from './Footer'
 import Navigation from './Navigation'
+import { FaSquareGithub, FaLinkedin } from 'react-icons/fa6'
 
 const Contact = () => {
 	// const { t } = useTranslation()
@@ -28,6 +29,24 @@ radial-gradient(circle at 30% -30%, #131213 0%, transparent 48%),
 					<h3 className=' text-white text-2xl sm:text-4xl lg:text-5xl 2xl:text-6xl 3xl:text-7xl font-bold tracking-wide '>
 						+48 512-374-866
 					</h3>
+					<div className='flex gap-x-6  mt-6'>
+						<motion.a
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							transition={{ delay: 0.9, duration: 0.7 }}
+							target='_blank'
+							href={'https://www.linkedin.com/in/m-gawlik/'}>
+							<FaLinkedin className='text-slate-100 w-10 h-10 md:w-12 md:h-12 3xl:w-16 3xl:h-16 hover:text-slate-200 hover:animate-pulse hover:scale-110 duration-300' />
+						</motion.a>
+						<motion.a
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							transition={{ delay: 0.9, duration: 0.7 }}
+							target='_blank'
+							href={'https://github.com/misiek869'}>
+							<FaSquareGithub className='text-slate-100 w-10 h-10 md:w-12 md:h-12 3xl:w-16 3xl:h-16 hover:text-slate-200 hover:animate-pulse hover:scale-110 duration-300' />
+						</motion.a>
+					</div>
 				</div>
 			</motion.section>
 			<motion.div
